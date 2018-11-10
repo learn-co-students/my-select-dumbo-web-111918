059a26fc04182ed2782509cdd5965de800819df6
John Mark Redding <johnmarkredding@gmail.com>
2018-11-10 05:12:01 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
- # your code here!
+  # your code here!
+  new_arr = []
+  if collection.empty?
+    new_arr = collection
+  else
+    collection.each do |x|
+      if yield x
+        new_arr << x
+      end
+    end
+  end
+  new_arr
 end
