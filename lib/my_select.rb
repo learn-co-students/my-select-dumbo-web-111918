73d003e11016ddcc739d4a81dbbede0bea5c4e92
luijocroussett <ljcroussett@gmail.com>
2018-11-16 14:29:52 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
-def my_select(collection)
- # your code here!
+def my_select(array_to_iterate)
+  new_array_selection = []
+  counter = 0
+  while counter < array_to_iterate.size
+    if yield(array_to_iterate[counter])
+      new_array_selection << array_to_iterate[counter]
+    end
+    counter += 1
+  end
+  new_array_selection
 end
